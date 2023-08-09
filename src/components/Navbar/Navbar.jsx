4ba30cf6fc684c2cb61300d020115ae9ad8c38fc
unknown <matheus.duartesilva42@gmail.com>
2023-08-09 19:49:34 -0300

    navbar implementada

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="left-side">
+                <h2>REACT STOCK</h2>
+            </div>
+            <div className="right-side">
+                <p href="">Início</p>
+                <p href="">Itens</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
